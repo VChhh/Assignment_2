b4046025f76d62abf61bf70e95b990c619eb309b
Ephil012 <Etp5501@gmail.com>
2022-03-25 13:25:37 -0400

    Removed interact key

--- Assets/Codes/Cabinet.cs
+++ Assets/Codes/Cabinet.cs
@@ -9,12 +9,10 @@ public class Cabinet : MonoBehaviour
     public GameObject key;
     private bool is_opended = false; // to make sure opening the gate for once
     public void Interact(){
-        if(Input.GetKey("f")) {
-            if(!is_opended){
-                interactable_part.transform.Rotate(0,-90,0); // open the cabinet gate
-                key.GetComponent<key>().is_collectable = true;
-            }
-            is_opended = is_opended | true;
+        if(!is_opended){
+            interactable_part.transform.Rotate(0,-90,0); // open the cabinet gate
+            key.GetComponent<key>().is_collectable = true;
         }
+        is_opended = is_opended | true;
     }
 }
