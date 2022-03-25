35da5766311be43cc73618807bd46ecf6e4dbc2e
reginauwu <wregina0418@gmail.com>
2022-03-24 22:50:12 -0400

    character asset

---
+++ Assets/Codes/old_key&gate_code/collections.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collections : MonoBehaviour
+{
+    public static bool prf_key_01 = false; //blue key
+    public static bool prf_key_02 = false; //black key
+    public static bool prf_key_03 = false; //green key
+    public static bool prf_key_04 = false; //red key
+
+}
