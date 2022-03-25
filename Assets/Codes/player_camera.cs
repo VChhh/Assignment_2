e18febaeb5dbc8338a8c975fcdab004518ad2ee0
reginauwu <wregina0418@gmail.com>
2022-03-24 20:29:24 -0400

    playerCamera script

---
+++ Assets/Codes/player_camera.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_camera : MonoBehaviour
+{
+     // Start is called before the first frame update
+    GameObject player;
+    Vector3 offset;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
