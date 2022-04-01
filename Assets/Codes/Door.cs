49608e25cd33e620b90a9c227bcb670e3d618c7a
VChhh <ql2015@nyu.edu>
2022-04-01 15:43:06 -0400

    a

--- Assets/Codes/Door.cs
+++ Assets/Codes/Door.cs
@@ -6,10 +6,32 @@ using UnityEngine.SceneManagement;
 public class Door : MonoBehaviour
 {
     public string levelToLoad = "Level_1";
+
+    bool is_in = false;
+
+    private void OnCollisionEnter(Collision other) {
+        if(other.transform.CompareTag("Player")){
+            is_in = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision other) {
+        if(other.transform.CompareTag("Player")){
+            is_in = false;
+        }
+    }
+
+    private void Update() {
+        print("1");
+        if(is_in) print("hi");
+        if(is_in && Input.GetKey("f")) {
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
     
     public void Interact(){
         if(Input.GetKey("f")) {
-            SceneManager.LoadScene(levelToLoad);
+            //SceneManager.LoadScene(levelToLoad);
         }
     }
 }
