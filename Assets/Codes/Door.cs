475007df7adb0be9b877b2af44b21d08ff03cab1
Ephil012 <Etp5501@gmail.com>
2022-04-01 16:00:32 -0400

    Cleaned up doors

--- Assets/Codes/Door.cs
+++ Assets/Codes/Door.cs
@@ -5,33 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
-    public string levelToLoad = "Level_1";
-
-    bool is_in = false;
-
-    private void OnCollisionEnter(Collision other) {
-        if(other.transform.CompareTag("Player")){
-            is_in = true;
-        }
-    }
-
-    private void OnCollisionExit(Collision other) {
-        if(other.transform.CompareTag("Player")){
-            is_in = false;
-        }
-    }
-
-    private void Update() {
-        print("1");
-        if(is_in) print("hi");
-        if(is_in && Input.GetKey("f")) {
-            SceneManager.LoadScene(levelToLoad);
-        }
-    }
-    
+    public string levelToLoad = "house";
     public void Interact(){
-        if(Input.GetKey("f")) {
-            //SceneManager.LoadScene(levelToLoad);
-        }
+        SceneManager.LoadScene(levelToLoad);
     }
 }
