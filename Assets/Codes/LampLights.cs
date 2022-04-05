8301fb4a753668237002c882b22319e62349a773
Ephil012 <Etp5501@gmail.com>
2022-04-05 18:47:20 -0400

    Added lamp code

---
+++ Assets/Codes/LampLights.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampLights : MonoBehaviour
+{
+    public int houseNumber = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PublicVars.houses_completed[houseNumber] == true)
+        {
+            this.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+        } else 
+        { 
+            this.gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        }
+    }
+
+    public void TurnOn()
+    {
+        this.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+        PublicVars.houses_completed[houseNumber] = true;
+    }
+}
--- Assets/Codes/PublicVars.cs
+++ Assets/Codes/PublicVars.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 public static class PublicVars{
     public static int score = 0;
 
-
     public static int keys_in_world = 1;
     public static int keys_on_player = 0;
+
+    public static bool[] houses_completed = new bool[9];
 }
 
