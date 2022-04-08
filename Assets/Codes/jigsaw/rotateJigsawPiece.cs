95a3d151ea8b8816b11c821c461022d5bf7ab43a
reginauwu <wregina0418@gmail.com>
2022-04-07 22:33:59 -0400

    jigsaw (press f)

--- Assets/Codes/jigsaw/rotateJigsawPiece.cs
+++ Assets/Codes/jigsaw/rotateJigsawPiece.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class rotateJigsawPiece : MonoBehaviour
 {
-    public Camera maincam;
+    // public Camera maincam;
+    bool is_in = false;
+    private Renderer _rd;
+
+     private void Start() {
+        _rd = GetComponent<Renderer>();
+    }
 
 
     void Update()
     {
-        maincam = Camera.main;
-        if(Input.GetMouseButtonDown(1)){
+        // maincam = Camera.main;
+        // if(Input.GetMouseButtonDown(1)){
+        if(is_in && Input.GetKeyDown("f")){
+            print('f');
             if (!jigsawManager.win) {
                 soundManagerScript.playSound("rotateSound");
                 rotate();
@@ -19,12 +27,25 @@ public class rotateJigsawPiece : MonoBehaviour
     }
 
     void rotate() {
-        RaycastHit hit;
-        if(Physics.Raycast(maincam.ScreenPointToRay(Input.mousePosition), out hit, 200))
-        {
-            if(hit.transform.CompareTag("Jigsaw")) {
-                hit.transform.Rotate(0f, 90f, 0f);
-            }
+        // RaycastHit hit;
+        // if(Physics.Raycast(maincam.ScreenPointToRay(Input.mousePosition), out hit, 200))
+        // {
+        //     if(hit.transform.CompareTag("Jigsaw")) {
+        //         hit.transform.Rotate(0f, 90f, 0f);
+        //     }
+        // }
+        _rd.transform.Rotate(0f, 90f, 0f);
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        if(other.transform.CompareTag("Player")){
+            is_in = true;
+            print("touch");
+        }
+    }
+    private void OnCollisionExit(Collision other) {
+        if(other.transform.CompareTag("Player")){
+            is_in = false;
         }
     }
 }
