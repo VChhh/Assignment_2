583a276e6d7fe8cf74f9c8511b62d605416ec1ef
reginauwu <wregina0418@gmail.com>
2022-04-08 13:21:18 -0400

    puzzle4 clue counter

--- Assets/Codes/maze_manager.cs
+++ Assets/Codes/maze_manager.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class maze_manager : MonoBehaviour
 {
+    public TextMeshProUGUI scoreUI;
     void Start()
     {
+        PublicVars.score += 1;
+        scoreUI.text = "Clues: " + PublicVars.score + "/8";
         PublicVars.keys_in_world = 2;
     }
 }
