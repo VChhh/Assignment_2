47b959df36b162467387a67adeb8bcc1b2e8ad86
VChhh <ql2015@nyu.edu>
2022-04-09 00:28:08 -0400

    grab animation

--- Assets/Codes/grab.cs
+++ Assets/Codes/grab.cs
@@ -10,6 +10,8 @@ public class grab : MonoBehaviour
     public Camera maincam;
     private int the_layer;
 
+    Animator _an;
+
     
     public TextMeshProUGUI scoreUI;
 
@@ -17,6 +19,7 @@ public class grab : MonoBehaviour
     {
         maincam = Camera.main;
         the_layer = LayerMask.GetMask("key");
+        _an = GetComponent<Animator>();
     }
 
     void Update()
@@ -78,15 +81,17 @@ public class grab : MonoBehaviour
         // check only the key layer
         // destroy the key and update the key num if the key is_collectable
         if(Physics.Raycast(maincam.ScreenPointToRay(Input.mousePosition), out grab_hit, 200, the_layer) 
-            && grab_hit.transform.GetComponent<key>().is_collectable){
+            && grab_hit.transform.GetComponent<key>().is_collectable 
+            && Vector3.Distance(transform.position, grab_hit.transform.position) < 5){
             PublicVars.keys_in_world -- ;
             PublicVars.keys_on_player ++ ;
-            Destroy(grab_hit.transform.gameObject);
+            StartCoroutine(wait_and_destory(grab_hit.transform.gameObject));
         }
-
-        
-        
     }
 
-
+    IEnumerator wait_and_destory(GameObject g){
+        _an.SetBool("grabbing", true);
+        yield return new WaitForSeconds(1f);
+        Destroy(g);
+    }
 }
